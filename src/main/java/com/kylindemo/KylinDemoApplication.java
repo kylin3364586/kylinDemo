f59693ad435697e94d0ed3e3a28f23977e2c72c5
kyin <m18380232959@163.com>
2023-12-26 17:12:04 +0800

    Initial commit

---
+++ src/main/java/com/kylindemo/KylinDemoApplication.java
@@ -0,0 +1,13 @@
+package com.kylindemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KylinDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KylinDemoApplication.class, args);
+    }
+
+}
